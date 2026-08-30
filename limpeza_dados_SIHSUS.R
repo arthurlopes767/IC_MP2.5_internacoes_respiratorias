a271343b463c1180efb67b5ae41275710952a2bb
arthurlopes767 <arthur.luizlopesaraujo@gmail.com>
2026-08-30 00:10:09 -0300

    chore(limpeza_dados_SIHSUS):mantem apenas dados do período de estudo

--- limpeza_dados_SIHSUS.R
+++ limpeza_dados_SIHSUS.R
@@ -1,7 +1,7 @@
 library(microdatasus)
 library(tidyverse)
 
-sih_rd <- readRDS("dados_SIH/dados_sih_sp_2021_2025.rds")
+sih_rd <- readRDS("dados_SIH/dados_sih_sp_2021_1s2026.rds")
 sih_proc <- process_sih(sih_rd) #Corrige os formatos dos dados e imputa dados sobre o munícipio de residência
 
 sih_tratado <- sih_proc |> 
@@ -21,17 +21,19 @@ sih_tratado <- sih_proc |>
     VAL_TOT = as.numeric(VAL_TOT),
     
     #Padroniza o indicador de óbito (1 = Sim, 0 = Não)
-    MORTE = as.integer(MORTE)
+    MORTE = if_else(MORTE == "Sim", 1L, 0L)
   )
 
-#Seleciona os CIDS e municípios de estudo
+#Seleciona os municípios e CIDs de estudo
 municipios <- c("350950", "352690", "353650")
 doencas <- c("J41", "J42", "J43", "J44", "J45")
 
 #Filtra os dados, removendo municípios, doenças e colunas não utilizadas no estudo
 sih_final <- sih_tratado |> 
   filter(MUNIC_RES %in% municipios) |> 
-  filter(substr(DIAG_PRINC, 1, 3) %in% doencas) |> 
-  select(-ANO_CMPT, -MES_CMTP, -munResStatus, -munResTipo) 
+  filter(substr(DIAG_PRINC, 1, 3) %in% doencas) |>
+  filter(DT_INTER >= "2021-01-01" & DT_INTER <= "2025-12-31")
+  select(-munResStatus, -munResTipo, -munResUf, -munResLat, -munResLon, 
+         -munResAlt, -munResArea)
 
-saveRDS(sih_final, "dados_SIH/sih_final_respiratorias")
+saveRDS(sih_final, "dados_SIH/sih_sp_tratado_respiratorias.rds")
